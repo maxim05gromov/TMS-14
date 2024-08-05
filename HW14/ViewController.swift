3bfc4672735877df08e3cb3eb3fa35d7c3b3c8d9
Maksim <i@mgromov.ru>
2024-08-05 21:11:36 +0300

    initial

--- HW14/ViewController.swift
+++ HW14/ViewController.swift
@@ -6,12 +6,30 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
+import SnapKit
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! myCollectionViewCell
+        cell.image.image = UIImage(systemName: devices[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return devices.count
+    }
+    
+    
+    
+    
+    let devices = ["airtag", "macpro.gen3", "macbook.gen2", "macstudio", "ipod", "iphone.gen3", "ipad.gen2", "applewatch.watchface", "tv"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collection.dataSource = self
+        collection.delegate = self
     }
 
 
---
+++ HW14/myCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  myCollectionViewCell.swift
+//  HW14
+//
+//  Created by Максим Громов on 03.08.2024.
+//
+
+import UIKit
+
+class myCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var image: UIImageView!
+    
+}
